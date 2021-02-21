7ae2aea5ad95ae0e6e9a847d23a76d7c1981c3a2
Neil Daftary <nmd278@nyu.edu>
2021-02-21 18:08:40 -0500

    Add TS support.

--- app/javascript/packs/hello_typescript.tsx
+++ app/javascript/packs/hello_typescript.tsx
@@ -4,21 +4,16 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
 
-const Hello = (props) => <div>Hello {props.name}!</div>;
-
-Hello.defaultProps = {
-  name: "David",
+type HelloTSProps = {
+  name: string;
 };
 
-Hello.propTypes = {
-  name: PropTypes.string,
-};
+const HelloTS: React.FC<HelloTSProps> = (props) => <div>Hello {props.name}!</div>;
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <HelloTS name="Typescript" />,
     document.body.appendChild(document.createElement("div"))
   );
 });
